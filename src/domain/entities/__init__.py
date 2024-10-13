9e8f735f4837aae2d6e111857f1fa2971cb741df
Eros Gabriel <eros.gabriel@ibasolutions.com.br>
2024-10-13 00:48:30 -0300

    feat(exceptions): adicionar exceções customizadas da aplicação

---
+++ src/domain/entities/__init__.py
@@ -0,0 +1,3 @@
+from src.domain.entities.users.PersonEntity import PersonEntity
+from src.domain.entities.users.UserEntity import UserEntity
+from src.domain.entities.projects.ProjectEntity import ProjectEntity
