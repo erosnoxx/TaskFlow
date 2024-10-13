5f9d482cba0e12370d74a8f96a67283f385bdd66
Eros Gabriel <eros.gabriel@ibasolutions.com.br>
2024-10-13 00:45:52 -0300

    chore: configurar ambiente inicial e arquivos de configuração

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from dynaconf import FlaskDynaconf
+
+
+class App:
+    def create_app(self) -> Flask:
+        app = Flask(__name__)
+        FlaskDynaconf(app=app, extensions_list=True)
+        return app
+
+app = App().create_app()
