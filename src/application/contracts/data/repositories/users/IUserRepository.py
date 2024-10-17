0b9dbf88865cf217320d7784715c5224ad0d3d68
Eros Gabriel <eros.gabriel@ibasolutions.com.br>
2024-10-17 18:23:44 -0300

    refactor(repositories): improve user repository logic

--- src/application/contracts/data/repositories/users/IUserRepository.py
+++ src/application/contracts/data/repositories/users/IUserRepository.py
@@ -4,4 +4,8 @@ from src.domain.entities.users.UserEntity import UserEntity
 
 
 class IUserRepository(ABC, BaseRepository[UserEntity]):
-    pass
+    def get_by_email(self, email: str) -> UserEntity:
+        pass
+    
+    def get_by_username(self, username: str) -> UserEntity:
+        pass
--- src/data/repositories/users/UserRepository.py
+++ src/data/repositories/users/UserRepository.py
@@ -6,3 +6,9 @@ from src.domain.entities.users.UserEntity import UserEntity
 class UserRepository(IUserRepository):
     def __init__(self, session: Session):
         super().__init__(session, UserEntity)
+
+    def get_by_email(self, email: str) -> UserEntity:
+        return self.session.query(self.entity).filter(self.entity.email==email).first()
+    
+    def get_by_username(self, username: str) -> UserEntity:
+        return self.session.query(self.entity).filter(self.entity.username==username).first()
