55947dfb5054734cd5aac188d832a1f654251e28
Eros Gabriel <eros.gabriel@ibasolutions.com.br>
2024-10-18 13:08:40 -0300

    feat(user): atualizar DTO para criação de usuário

--- src/domain/dto/user/input/CreateUserInputDto.py
+++ src/domain/dto/user/input/CreateUserInputDto.py
@@ -1,4 +1,7 @@
-class CreateUserInputDto:
+from src.domain.dto.common.BaseInputDto import BaseInputDto
+
+
+class CreateUserInputDto(BaseInputDto):
     required_fields = ['username', 'email', 'password']
     def __init__(self,
                  username: str=None,
@@ -7,8 +10,3 @@ class CreateUserInputDto:
         self.username = username
         self.email = email
         self.password = password
-
-    def set_attributes(self, **kwargs) -> None:
-        for key, value in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
