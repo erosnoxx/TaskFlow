06e87419c52dc29d561a4fca051c8798827b6fda
Eros Gabriel <eros.gabriel@ibasolutions.com.br>
2024-10-18 13:08:28 -0300

    fix(errors): atualizar esquemas de erro

--- src/controllers/schemas/common/ErrorSchemas.py
+++ src/controllers/schemas/common/ErrorSchemas.py
@@ -1,9 +1,11 @@
 from flask import make_response
 from flask_restx import fields, Model
 from src.application.extensions.Api import api
-from src.exceptions.users.InvalidFieldException import InvalidFieldException
-from src.exceptions.users.PersonNotExistsException import PersonNotExistsException
-from src.exceptions.users.UserAlreadyExistsException import UserAlreadyExistsException
+from src.exceptions.http.BadRequestException import BadRequestException
+from src.exceptions.http.ConflictException import ConflictException
+from src.exceptions.http.InternalServerException import InternalServerException
+from src.exceptions.http.NotFoundException import NotFoundException
+from src.exceptions.http.UnauthorizedException import UnauthorizedException
 
 
 class ErrorSchemas:
@@ -42,6 +44,15 @@ class ErrorSchemas:
             'details': fields.String,
             'statuscode': fields.Integer(default=409)
         })
+    
+    @staticmethod
+    def http_401() -> Model:
+        return api.model('Error 401', {
+            'success': fields.Boolean(default=False),
+            'message': fields.String,
+            'details': fields.String,
+            'statuscode': fields.Integer(default=401)
+        })
 
     @staticmethod
     def make_error_response(status_code: int, message: str, details: str=None):
@@ -60,21 +71,31 @@ class ErrorSchemas:
         def wrapper(*args, **kwargs):
             try:
                 return f(*args, **kwargs)
-            except InvalidFieldException as ife:
+            except BadRequestException as bre:
                 return ErrorSchemas.make_error_response(
                     status_code=400,
-                    message='Um ou mais campos fornecidos são inválidos.',
-                    details=str(ife))
-            except PersonNotExistsException as pne:
-                return ErrorSchemas.make_error_response(
-                    status_code=404,
-                    message='Erro ao criar usuário.',
-                    details=str(pne))
-            except UserAlreadyExistsException as uae:
+                    message='Erro interno no servidor',
+                    details=str(bre))
+            except ConflictException as ce:
                 return ErrorSchemas.make_error_response(
                     status_code=409,
-                    message='Usuário já registrado com os dados fornecidos.',
-                    details=str(uae))
+                    message='Erro interno no servidor',
+                    details=str(ce))
+            except NotFoundException as nfe:
+                return ErrorSchemas.make_error_response(
+                    status_code=404,
+                    message='Erro interno no servidor',
+                    details=str(nfe))
+            except UnauthorizedException as ue:
+                return ErrorSchemas.make_error_response(
+                    status_code=401,
+                    message='Erro interno no servidor',
+                    details=str(ue))
+            except InternalServerException as ise:
+                return ErrorSchemas.make_error_response(
+                    status_code=500,
+                    message='Erro interno no servidor',
+                    details=str(ise))
             except Exception as e:
                 return ErrorSchemas.make_error_response(
                     status_code=500,
