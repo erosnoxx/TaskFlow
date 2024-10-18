b19fbb2df4d5f538eff217bb8fb1edfc4f0c1de6
Eros Gabriel <eros.gabriel@ibasolutions.com.br>
2024-10-18 13:07:03 -0300

    docs(auth): atualizar documentação para autenticação

---
+++ src/controllers/schemas/auth/LoginSchemas.py
@@ -0,0 +1,36 @@
+from flask import Response, make_response
+from flask_restx import Model, fields
+from src.application.extensions.Api import api
+
+
+class LoginSchemas:
+    @staticmethod
+    def login_input_schema() -> Model:
+        return api.model('Login User Input', {
+            'username': fields.String(required=False, description='Nome de usuário para login.'),
+            'email': fields.String(
+                required=False,
+                description='Endereço de e-mail do usuário.',
+                default='example@email.com'),
+            'password': fields.String(required=True,
+                                      description='Senha do usuário.', min_length=8, max_length=255,
+                                      default='SenhaForte5@')})
+
+    @staticmethod
+    def login_output_schema() -> Model:
+        return api.model('Login User Output', {
+            'success': fields.Boolean(description='Indica se o login foi bem-sucedido.'),
+            'statuscode': fields.Integer(default=200, description='Código de status HTTP do resultado da operação.'),
+            'message': fields.String(description='Mensagem informativa sobre o resultado do login.'),
+            'id': fields.Integer(description='ID do usuário.')})
+
+    @staticmethod
+    def make_success_response(user_id: int) -> Response:
+        response_data = {
+                'success': True,
+                'statuscode': 200,
+                'message': 'Usuário logado com sucesso.',
+                'id': user_id}
+        response = make_response(response_data, 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
