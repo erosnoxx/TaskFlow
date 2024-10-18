84c2bce8bd4455714e111a091234228761f47004
Eros Gabriel <eros.gabriel@ibasolutions.com.br>
2024-10-18 13:08:03 -0300

    chore(management): refatorar e atualizar a extensão de gerenciamento

--- src/application/extensions/Management.py
+++ src/application/extensions/Management.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+lm = LoginManager()
+
 
 class Management:
     def __init__(self, app: Flask) -> None:
