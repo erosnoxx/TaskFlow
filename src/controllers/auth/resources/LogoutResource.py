e837199e4f782eb78da1d72722f983733a361e67
Eros Gabriel <eros.gabriel@ibasolutions.com.br>
2024-10-18 13:06:39 -0300

    feat(auth): implementar funcionalidade de logout

---
+++ src/controllers/auth/resources/LogoutResource.py
@@ -0,0 +1,30 @@
+from flask import make_response
+from flask_login import logout_user, current_user
+from flask_restx import Resource
+
+
+class LogoutResource(Resource):
+    def delete(self) -> None:
+        if not current_user.is_authenticated:
+            response_data = {
+                'success': False,
+                'statuscode': 401,
+                'message': 'Usuário não está autenticado.'
+            }
+            response = make_response(response_data, 401)
+
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+        user_id = current_user.id
+        logout_user()
+        
+        response_data = {
+            'success': True,
+            'statuscode': 200,
+            'message': 'Usuário deslogado com sucesso.',
+            'id': user_id
+        }
+        response = make_response(response_data, 200)
+
+        return response
