f03e9b6666169032bad4241c36d3b235548adddc
Eros Gabriel <eros.gabriel@ibasolutions.com.br>
2024-10-19 21:49:11 -0300

    fix(services): corrigir lógica nos serviços de usuário

--- src/infra/services/users/UserService.py
+++ src/infra/services/users/UserService.py
@@ -1,3 +1,4 @@
+from typing import List
 from src.application.contracts.data.repositories.users.IUserRepository import IUserRepository
 from src.domain.entities.users.UserEntity import UserEntity
 
@@ -18,8 +19,14 @@ class UserService:
             password_hash=password_hash)
         return self.user_repository.add(obj=user_entity)
 
-    def find_email(self, email: str) -> UserEntity:
+    def find_by_email(self, email: str) -> UserEntity:
         return self.user_repository.get_by_email(email=email)
 
-    def find_username(self, username: str) -> UserEntity:
+    def find_by_username(self, username: str) -> UserEntity:
         return self.user_repository.get_by_username(username=username)
+    
+    def find_by_id(self, user_id: int) -> UserEntity:
+        return self.user_repository.get(obj_id=user_id)
+    
+    def find_all_users(self) -> List[UserEntity]:
+        return self.user_repository.get_all()
